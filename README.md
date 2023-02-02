3e531fd965ec9c905abb4b576018b2482642b3fe
정선우 <laborlawseon@gmail.com>
2023-02-02 21:45:26 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # intellij-guide
 IntelliJ를 시작하시는 분들을 위한 IntelliJ 가이드
+
+- 현재 포커스 실행 
+  - Ctrl + Shift + R
+  
+- 이전 실행
+  - Shift + F10
--- src/main/java/com/seonwoo/inflearn/intellij/Main2.java
+++ src/main/java/com/seonwoo/inflearn/intellij/Main2.java
@@ -1,7 +1,7 @@
 package com.seonwoo.inflearn.intellij;
 
-public class Main {
+public class Main2 {
 	public static void main(String[] args) {
-		System.out.println("aaa");
+		System.out.println("bbb");
 	}
 }
