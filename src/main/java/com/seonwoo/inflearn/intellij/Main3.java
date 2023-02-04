8c53399df81fac6c3f353ca5f66801b7d7467450
정선우 <laborlawseon@gmail.com>
2023-02-04 23:47:21 +0900

    commit

--- src/main/java/com/seonwoo/inflearn/intellij/Main3.java
+++ src/main/java/com/seonwoo/inflearn/intellij/Main3.java
@@ -4,6 +4,11 @@ public class Main3 {
 
 	public void copyLine() {
 		System.out.println("현재 줄을 복사합니다.");
+		System.out.println("현재 줄을 복사합니다.");
+		System.out.println("현재 줄을 복사합니다.");
+		System.out.println("현재 줄을 복사합니다.");
+		System.out.println("현재 줄을 복사합니다.");
+		System.out.println("현재 줄을 복사합니다.");
 
 	}
 }
